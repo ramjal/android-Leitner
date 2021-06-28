258389d1d09b300889f9c5abe0ce88d17ea62ba5
ramjal <ramjal@gmail.com>
2021-06-28 16:25:54 -0700

    Adding notification

--- app/src/main/java/com/example/leitner/notification/NotificationUtils.kt
+++ app/src/main/java/com/example/leitner/notification/NotificationUtils.kt
@@ -75,7 +75,6 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     notify(NOTIFICATION_ID, builder.build())
 }
 
-// TODO: Step 1.14 Cancel all notifications
 /**
  * Cancels all notifications.
  *
